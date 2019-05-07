75e3c35c4c82ae24109170e62332f4ee320f52ff
Ana <anaradojevic90@gmail.com>
2019-05-07 09:26:04 +0200

    Added class television

---
+++ src/media/Television.java
@@ -0,0 +1,11 @@
+
+package media;
+
+public class Television {
+    private int volume;
+    private int currentprogram;
+    private boolean turnOn;
+    
+    public void setVolume(int newVolume){}
+    this.volume 
+}
