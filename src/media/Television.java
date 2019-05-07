e4211cef9252d315dd9958f8b9c0d2809dc07335
Ana <anaradojevic90@gmail.com>
2019-05-07 09:31:11 +0200

    Added class radio

--- src/media/Television.java
+++ src/media/Television.java
@@ -3,9 +3,28 @@ package media;
 
 public class Television {
     private int volume;
-    private int currentprogram;
+    private int currentProgram;
     private boolean turnOn;
     
-    public void setVolume(int newVolume){}
-    this.volume 
+    public void setVolume(int newVolume){
+     this.volume = newVolume;
+    }
+   public int getVolume(){
+     return this.volume;
+    }
+   
+    public void setcurrentProgram(int currentProgram ){
+     this.currentProgram = currentProgram;
+    }
+   public int getcurrentProgram(){
+     return this.currentProgram;
+    }
+   
+    public void setTurnOn(boolean turnOn){
+     this.turnOn= turnOn;
+    }
+   public boolean getturnOn(){
+     return this.turnOn;
+    }
+   
 }
